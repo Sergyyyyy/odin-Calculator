ce0c801f99d6ca6815b56e4bb0c3fbe2e181e2e3
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-02-07 12:37:24 +0800

    Fixing logic for operators

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 let allBtns = document.querySelectorAll(".calc-btns")
-let allOpBtns = document.querySelectorAll(".operator-btns")
 let input = document.getElementById("calc-input")
 
-function add(x, y){
+function add(x, y) {
     return x + y
 }
 
-function subtract(x, y){
+function subtract(x, y) {
     return x - y
 }
 
-function multiply(x, y){
+function multiply(x, y) {
     return x * y
 }
 
-function divide(x, y){
+function divide(x, y) {
     return x / y
 }
 
-function operate(operator, x, y){
-    
+function operate(operator, x, y) {
+    switch (operator) {
+        case "+":
+            return add(x, y)
+            break;
+        case "-":
+            return subtract(x, y)
+            break;
+        case "*":
+            return multiply(x, y)
+            break;
+        case "/":
+            return divide(x, y)
+            break;
+    }
 }
 
 for (let i = 0; i <= allBtns.length; i++) {
@@ -27,6 +39,13 @@ for (let i = 0; i <= allBtns.length; i++) {
         if (allBtns[i].innerHTML == "clear") {
             document.getElementById("clear-btn").onclick = function () {
                 input.value = ""
+            }
+            if (allBtns[i].innerHTML == "enter") {
+                document.getElementById("enter-btn").onclick = function () {
+                    let currentOp = allBtns[i]
+                    let value = operate(currentOp, allBtns[i], allBtns[i])
+                    input.value += value
+                }
             }
         }
         input.value += allBtns[i].innerHTML
